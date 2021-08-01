96c4910e0af9725d6d0b90f48cba18a9d78a15b1
Herko <herko@index.hu>
2021-08-01 16:10:38 +0200

    Initial commit

---
+++ src/test/java/productmanager/ProductManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package productmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
